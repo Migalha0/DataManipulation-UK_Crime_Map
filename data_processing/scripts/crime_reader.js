a9058ae252b0a6e0c639b0e08e908b25a50c387e
Gabriel Magalhaes <gabrielmigalhabarros@gmail.com>
2026-07-06 14:10:01 -0300

    refactor: geojson calucation in the node backend finished

--- data_processing/scripts/crime_reader.js
+++
@@ -1,7 +0,0 @@
-import fs from 'node:fs';
-import path from 'node:path';
-
-const test_folder = '../data_raw/crime/2023-05';
-const contents = fs.readdirSync(test_folder);
-
-console.log(contents);
--- front_end/src/scripts/leaflet_map.js
+++ front_end/src/scripts/leaflet_map.js
@@ -1,7 +1,7 @@
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet';
 import * as turf from '@turf/turf'
-import {load_data} from './data_reader'
+import geojson_data from '../data/LSOA_2021_processed.json'
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //             MAP SETUP
@@ -28,35 +28,44 @@ L.tileLayer(tilemap_esri).addTo(map);
 //             POLYGONS
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-const data = await load_data();
 
 // normalizing population to better see population distribution over land
-const populations = data.features.map(
-    feature => feature.properties.population
+// const populations = geojson_data.features.map(
+//     feature => feature.properties.Population
+// );
+const crimes = geojson_data.features.map(
+    feature => feature.properties.Crime_count ?? 0
 );
 
-const min_population = Math.min(...populations);
-const max_population = Math.max(...populations);
+// const min_population = Math.min(...populations);
+// const max_population = Math.max(...populations);
+const max_crime = Math.max(...crimes);
 
 // styling color according to pop count
-L.geoJSON(data, {
+L.geoJSON(geojson_data, {
     style: feature => {
 
         // get population normalize and color accordingly
-        const population = feature.properties.population;
+        // const population = feature.properties.Population;
 
-        const population_color_normalized =
-            Math.sqrt(
-                (population - min_population) /
-                (max_population - min_population)
-            );
+        // const population_color_normalized =
+        //     Math.sqrt(
+        //         (population - min_population) /
+        //         (max_population - min_population)
+        //     );
+
+        const crime = feature.properties.Crime_count ?? 0;
+
+        const crime_color_normalized = Math.sqrt(
+            (crime)/(max_crime)
+        );
 
         return {
             
             color: '#000000',
             weight: 0.3,
             fillColor:'#d80c0c',
-            fillOpacity: population_color_normalized
+            fillOpacity: crime_color_normalized
         };
     }
 }).addTo(map);
@@ -68,21 +77,23 @@ L.geoJSON(data, {
 function onMapClick(e){
     // get point
     // search polygon which had point inside
-    const point = turf.point([e.latlng.lng,e.latlng.lat])    
+    const point = turf.point([e.latlng.lng,e.latlng.lat])
+
     let polygon_data = null
     
-    for (const feature of data.features){
+    for (const feature of geojson_data.features){
         if(turf.booleanPointInPolygon(point,feature)){
             polygon_data = feature.properties;
             break;
         }
     }
+    if(!polygon_data){
+        return
+    }
     
     // print info
     const marker = L.marker(e.latlng).addTo(map);
-    marker
-        .bindPopup(`${e.latlng} <br> ${polygon_data.district} ${polygon_data.population}`)
-        .openPopup();
+    marker.bindPopup(`${e.latlng} <br> ${polygon_data.LSOA21NM} <br> population: ${polygon_data.Population}<br>crime: ${polygon_data.Crime_count}`).openPopup();
 
     marker.on('click', () => marker.remove())
 }
